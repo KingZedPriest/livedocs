134a2037bb8185b966eed3b90f92b36b0a209790
Priest <Charleschukwuemeka47@gmail.com>
2024-07-31 12:26:55 +0100

    Feat: Done with the prelimiary set ups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
+ 
+import { cn } from "@/lib/utils"
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 import { ThemeProvider } from "@/components/theme-provider"
 import "../styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={cn( "min-h-screen bg-background font-sans antialiased", fontSans.variable )}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
