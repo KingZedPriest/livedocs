fecd8fc94f53e44352eac731f148a40872318693
Priest <Charleschukwuemeka47@gmail.com>
2024-08-02 18:01:28 +0100

    Feat: Add the authentications clerk buttons

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,18 +1,27 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+
 //Import Needed Components
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 
 const page = () => {
-    return ( 
+    return (
         <main>
             <Header>
                 <div className="flex w-fit items-center justify-center gap-2">
                     <p className="document.title">This is a fake document title</p>
                 </div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </Header>
             <Editor />
         </main>
-     );
+    );
 }
- 
+
 export default page;
