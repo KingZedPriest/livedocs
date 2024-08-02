4d6af126c6b3806f4aad12a1ef82c96dc3c15225
Priest <Charleschukwuemeka47@gmail.com>
2024-08-02 17:35:48 +0100

    Feat: Install clerk, add it's middleware

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,15 @@
 //Import Needed Components
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 const page = () => {
     return ( 
         <main>
+            <Header>
+                <div className="flex w-fit items-center justify-center gap-2">
+                    <p className="document.title">This is a fake document title</p>
+                </div>
+            </Header>
             <Editor />
         </main>
      );
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,79 +2,79 @@
 declare type SearchParamProps = {
     params: { [key: string]: string };
     searchParams: { [key: string]: string | string[] | undefined };
-  };
-  
-  declare type AccessType = ["room:write"] | ["room:read", "room:presence:write"];
-  
-  declare type RoomAccesses = Record<string, AccessType>;
-  
-  declare type UserType = "creator" | "editor" | "viewer";
-  
-  declare type RoomMetadata = {
+};
+
+declare type AccessType = ["room:write"] | ["room:read", "room:presence:write"];
+
+declare type RoomAccesses = Record<string, AccessType>;
+
+declare type UserType = "creator" | "editor" | "viewer";
+
+declare type RoomMetadata = {
     creatorId: string;
     email: string;
     title: string;
-  };
-  
-  declare type CreateDocumentParams = {
+};
+
+declare type CreateDocumentParams = {
     userId: string;
     email: string;
-  };
-  
-  declare type User = {
+};
+
+declare type User = {
     id: string;
     name: string;
     email: string;
     avatar: string;
     color: string;
     userType?: UserType;
-  };
-  
-  declare type ShareDocumentParams = {
+};
+
+declare type ShareDocumentParams = {
     roomId: string;
     email: string;
     userType: UserType;
     updatedBy: User;
-  };
-  
-  declare type UserTypeSelectorParams = {
+};
+
+declare type UserTypeSelectorParams = {
     userType: string;
     setUserType: React.Dispatch<React.SetStateAction<UserType>>;
     onClickHandler?: (value: string) => void;
-  };
-  
-  declare type ShareDocumentDialogProps = {
+};
+
+declare type ShareDocumentDialogProps = {
     roomId: string;
     collaborators: User[];
     creatorId: string;
     currentUserType: UserType;
-  };
-  
-  declare type HeaderProps = {
+};
+
+declare type HeaderProps = {
     children: React.ReactNode;
     className?: string;
-  };
-  
-  declare type CollaboratorProps = {
+};
+
+declare type CollaboratorProps = {
     roomId: string;
     email: string;
     creatorId: string;
     collaborator: User;
     user: User;
-  };
-  
-  declare type CollaborativeRoomProps = {
+};
+
+declare type CollaborativeRoomProps = {
     roomId: string;
     roomMetadata: RoomMetadata;
     users: User[];
     currentUserType: UserType;
-  };
-  
-  declare type AddDocumentBtnProps = {
+};
+
+declare type AddDocumentBtnProps = {
     userId: string;
     email: string;
-  };
-  
-  declare type DeleteModalProps = { roomId: string };
-  
-  declare type ThreadWrapperProps = { thread: ThreadData<BaseMetadata> };
+};
+
+declare type DeleteModalProps = { roomId: string };
+
+declare type ThreadWrapperProps = { thread: ThreadData<BaseMetadata> };
