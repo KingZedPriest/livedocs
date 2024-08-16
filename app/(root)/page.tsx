518565ed33d894e4c5e1b566d577759133e370c4
Priest <Charleschukwuemeka47@gmail.com>
2024-08-16 16:02:54 +0100

    Feat: Start the document frontpage code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,34 @@
+import Image from "next/image";
+
+//Import Needed Components
+import Header from "@/components/Header";
+
+//Import Needed Clerk Utils
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>This is the main page</h1>
 
+  const documents = [];
+
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div>
+
+        </div>
+      ) : (
+        <div className="document-list-empty">
+          <Image src="/assets/icons/doc.svg" alt="Document" className="mx-auto" width={40} height={40} />
+        </div>
+      )}
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
-//Import Needed Types
+//Import Needed Utils
+import { cn } from "@/lib/utils";
 
-const Header = ({children}: HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
     return (
-        <main className="header">
+        <main className={cn("header", className)}>
             <Link href="/" className="md:flex-1">
                 <Image src="/assets/icons/logo.svg" alt="Logo with name" width={120} height={32} className="hidden md:block" />
                 <Image src="/assets/icons/logo-icon.svg" alt="Logo" width={32} height={32} className="mr-2 md:hidden" />
